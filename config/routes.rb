aec3ea8b86447f96aca65882884f921905402460
clemence ogeron <ogeron.clemence@orange.fr>
2021-11-22 18:34:24 +0100

    complete routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,24 +2,20 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resource :dashboard, only[:show]
+  resource :dashboard, only: [:show]
 
-  resources :dictators, only:[:index, :show] do
-    resources :bookings, only:[:create]
+  resources :dictators, only: [:index, :show] do
+    resources :bookings, only: [:create]
   end
 
   namespace :owner do
-    resources :bookings do
+    resources :bookings, only: [] do
       member do
         patch :decline
         patch :accept
       end
     end
-    resources :dictators do
-      member do
-        patch :new
-        patch :create
-      end
-    end
+
+    resources :dictators, only: [:new, :create, :index]
   end
 end
