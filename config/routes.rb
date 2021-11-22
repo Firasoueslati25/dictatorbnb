22e89c3bf59ba51ed3ed2137ac513495334386ee
clemence ogeron <ogeron.clemence@orange.fr>
2021-11-22 18:15:22 +0100

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,25 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resource :dashboard, only[:show]
+
+  resources :dictators, only:[:index, :show] do
+    resources :bookings, only:[:create]
+  end
+
+  namespace :owner do
+    resources :bookings do
+      member do
+        patch :decline
+        patch :accept
+      end
+    end
+    resources :dictators do
+      member do
+        patch :new
+        patch :create
+      end
+    end
+  end
 end
