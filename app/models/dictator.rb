5ffcada430b7db4a5fb41944101c9fa6c0b6a24f
Firas Oueslati <firas.oueslati@audencia.com>
2021-11-23 23:23:58 +0100

    belongs_to optionnal true

--- app/models/dictator.rb
+++ app/models/dictator.rb
@@ -1,5 +1,5 @@
 class Dictator < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
 
   has_many :bookings
   has_many :reviews_as_dictator, through: :bookings, source: :reviews
