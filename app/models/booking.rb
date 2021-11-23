89c1aa6211dce14258b3b9abce53d351fcb04920
Firas Oueslati <firas.oueslati@audencia.com>
2021-11-23 11:03:02 +0100

    model terminé

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,5 @@
 class Booking < ApplicationRecord
-  validates :name, :address, presence: :true
+  validates :start_date, :end_date, presence: true
   belongs_to :users, :dictators
   has_many :reviews, dependent: :destroy
 end
--- db/migrate/20211123095034_create_bookings.rb
+++ db/migrate/20211123095034_create_bookings.rb
@@ -1,7 +1,10 @@
 class CreateBookings < ActiveRecord::Migration[6.0]
   def change
     create_table :bookings do |t|
-
+      t.date :start_date
+      t.date :end_date
+      t.string :status
+      t.integer :total_price
       t.timestamps
     end
   end
