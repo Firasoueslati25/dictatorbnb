b19e226ef20dd408e34620a245ae0bb1ffc29b8a
Firas Oueslati <firas.oueslati@audencia.com>
2021-11-23 14:47:23 +0100

    WIP dictators index

--- app/controllers/dictators_controller.rb
+++ app/controllers/dictators_controller.rb
@@ -2,28 +2,4 @@ class DictatorsController < ApplicationController
   def index
     @dictators = Dictator.all
   end
-
-  def show
-    @dictator = Dictator.find(params[:id])
-    @review = Review.new
-  end
-
-  def new
-    @dictator = Dictator.new
-  end
-
-  def create
-    @dictator = Dictator.new(dictator_params)
-    if @dictator.save
-      redirect_to dictator_path(@dictator)
-    else
-      render "new"
-    end
-  end
-
-  private
-
-  def dictator_params
-    params.require(:dictator).permit(:name, :description, :price_per_day)
-  end
 end
